20e280351066fcbe496d34b0683609741462b4d4
Chi-Hung, Chuang <57139283+chihong-j@users.noreply.github.com>
2023-11-07 17:14:25 +0800

    add models

---
+++ frontend/src/models/user.model.ts
@@ -0,0 +1,12 @@
+export type passenger = {
+    userName: string;
+    isMale: boolean;
+    // to be added
+};
+
+export type driver = {
+    userName: string;
+    isMale: boolean;
+    // to be added
+};
+  
