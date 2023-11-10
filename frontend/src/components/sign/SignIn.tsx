19e8c3025a9deb918b2f9f293c326a52a1cef9ca
Chi-Hung, Chuang <57139283+chihong-j@users.noreply.github.com>
2023-11-10 16:56:50 +0800

    implement signin feature

--- frontend/src/components/sign/SignIn.tsx
+++ frontend/src/components/sign/SignIn.tsx
@@ -3,28 +3,98 @@ import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { userSignIn } from "../../apis/sign.api";
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 type SignInProps = {
+    errorMessage: string;
+
+    setErrorMessage: (message: string) => void;
     setIsSignIn: (status: boolean) => void;
 }
 
 export const SignIn = (props: SignInProps) => {
-    const { setIsSignIn } = props;
+    const { setIsSignIn, errorMessage, setErrorMessage } = props;
     const navigate = useNavigate()
-    const logIn = () => {
+    const handleSignIn = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            username: data.get('username'),
+            password: data.get('password'),
+        });
         try {
-            // let response = await userSignIn("test");
+            // TODO: Error handling
+            // TODO: password hashing
+            // await userSignIn(data);
             navigate('/home', { state: { isDriver: false, name: 'Joey' }})
         }
         catch (error) {
             console.log(error);
         }
-    }
+    };
+
     return (
-        <div>
-            Sign In Component
-            <Button variant="contained" onClick={logIn}>Login</Button>
-            <Button variant="contained" onClick={()=>setIsSignIn(false)}>Go to Register</Button>
-        </div>
+        <>
+            <Typography component="h1" variant="h6" color="primary">
+                Sign in
+            </Typography>
+            <Box component="form" onSubmit={handleSignIn} noValidate sx={{ mt: 1 }}>
+                User Name
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="username"
+                    label="User Name"
+                    name="username"
+                    autoComplete="username"
+                    sx={{ mb: 1.5, mt: 1 }}
+                    autoFocus
+                />
+                Password
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    sx={{ mb: 1.5, mt: 1 }}
+                />
+                <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" size="small" />}
+                    label="Remember me"
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="secondary"
+                    sx={{ mb: 1, mt: 1 }}
+                >
+                    Login
+                </Button>
+                <Grid container>
+                    <Grid item>
+                        <p>
+                            {"Don't have an account? "}
+                            <b onClick={()=>setIsSignIn(false)}>Register</b>
+                        </p>
+                    </Grid>
+                </Grid>
+                <Grid item xs>
+                    <Typography sx={{ marginTop: 1,marginBottom: 2, color: errorMessage === "None" ? "white" : "error.main" }} >
+                        {errorMessage}                            
+                    </Typography>
+                </Grid>
+            </Box>
+        </>
     );
 }
