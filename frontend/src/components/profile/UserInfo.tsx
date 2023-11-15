88e6d9679bc0ef96a6805ae4e4d6fb56a47616a3
qingyun <qingyun032@gmail.com>
2023-11-15 16:08:53 +0800

    enable to edit UserInfo

--- frontend/src/components/profile/UserInfo.tsx
+++ frontend/src/components/profile/UserInfo.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useRef, RefObject } from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
 import { styled } from '@mui/material/styles';
 
 
@@ -16,6 +17,10 @@ const MidButton = styled(Button)({
 
 const Text = styled(TextField)({
   width: "275px"
+})
+
+const SelectItem = styled(MenuItem)({
+  fontSize: "14px"
 })
 
 type UserInfoProps = {
@@ -38,6 +43,57 @@ type UserInfoProps = {
 export const UserInfo = (props: UserInfoProps) => {
   const { setStatus, User } = props;
   const [ readonly, setReadonly ] = useState<boolean>(true);
+  const [ buttonText, setButtonText ] = useState<string>("Edit");
+  const [ user, setUser ] = useState(User);
+  const refs: { [key:string]: RefObject<HTMLDivElement> } = {
+    name: useRef<HTMLDivElement>(null),
+    email: useRef<HTMLDivElement>(null),
+    phone: useRef<HTMLDivElement>(null),
+    gender: useRef<HTMLDivElement>(null),
+    home: useRef<HTMLDivElement>(null),
+    company: useRef<HTMLDivElement>(null),
+    wallet: useRef<HTMLDivElement>(null),
+  }
+  const textMap = [
+    {id: "name", label: "User name", user: user.name},
+    {id: "email", label: "Email", user: user.email},
+    {id: "phone", label: "Phone number", user: user.phone},
+    {id: "gender", label: "Gender", user: user.gender},
+    {id: "home", label: "Home address", user: user.home},
+    {id: "company", label: "Company address", user: user.company},
+    {id: "wallet", label: "Wallet", user: user.wallet}
+  ]
+
+  const inputProps = {
+    readOnly: readonly,
+    style: {
+      fontSize: "14px"
+    }
+  }
+
+  const buttonClick = () => {
+    if(buttonText === "Edit"){
+      setButtonText("Update");
+      setReadonly(false);
+    }else{
+      setButtonText("Edit");
+      setReadonly(true);
+      setUser({
+        name: refs["name"].current?.getElementsByTagName("input")[0].value ?? user.name,
+        email: refs["email"].current?.getElementsByTagName("input")[0].value ?? user.email,
+        phone: refs["phone"].current?.getElementsByTagName("input")[0].value ?? user.phone,
+        gender: refs["gender"].current?.getElementsByTagName("input")[0].value ?? user.gender,
+        home: refs["home"].current?.getElementsByTagName("input")[0].value ?? user.home,
+        company: refs["company"].current?.getElementsByTagName("input")[0].value ?? user.company,
+        wallet: refs["wallet"].current?.getElementsByTagName("input")[0].value ?? user.wallet,
+        start: user.start,
+        destination: user.destination,
+        time: user.time,
+        people: user.people
+      })
+    }
+  }
+
   return (
     <>
       <Typography component="h1" variant="h5" color="primary">
@@ -49,87 +105,38 @@ export const UserInfo = (props: UserInfoProps) => {
           alignItems: "center",
           flexDirection: "column",
           justifyContent: 'space-evenly',
-          // border: "solid",
-          // borderColor: "#e0e0e0",
-          // borderRadius: "10px",
           pt: "20px",
           pb: "20px",
           minHeight: "75vh"
         }}
       >
-        <Text
-          id="email"
-          label="Email"
-          defaultValue={User.email}
-          variant="standard"
-          InputProps={{
-            readOnly: readonly,
-            style: {
-              fontSize: "14px"
-            }
-          }}
-        />
-        <Text
-          id="phone"
-          label="Phone number"
-          defaultValue={User.phone}
-          variant="standard"
-          InputProps={{
-            readOnly: readonly,
-            style: {
-              fontSize: "14px"
-            }
-          }}
-        />
-        <Text
-          id="gender"
-          label="Gender"
-          defaultValue={User.gender}
-          variant="standard"
-          InputProps={{
-            readOnly: readonly,
-            style: {
-              fontSize: "14px"
-            }
-          }}
-        />
-        <Text
-          id="home"
-          label="Home address"
-          defaultValue={User.home}
-          variant="standard"
-          InputProps={{
-            readOnly: readonly,
-            style: {
-              fontSize: "14px"
-            }
-          }}
-        />
-        <Text
-          id="company"
-          label="Company address"
-          defaultValue={User.company}
-          variant="standard"
-          InputProps={{
-            readOnly: readonly,
-            style: {
-              fontSize: "14px"
-            }
-          }}
-        />
-        <Text
-          id="wallet"
-          label="Wallet"
-          defaultValue={User.wallet}
-          variant="standard"
-          InputProps={{
-            readOnly: readonly,
-            style: {
-              fontSize: "14px"
-            }
-          }}
-        />
-        <MidButton variant="contained" >Edit</MidButton>
+        {textMap.map(({ id, label, user }) => (
+          (id === "gender")? 
+          <Text
+            select
+            id={id}
+            label={label}
+            defaultValue={user}
+            ref={refs[id]}
+            variant="standard"
+            InputProps={inputProps}
+          >
+            <SelectItem value={"Male"}>Male</SelectItem>
+            <SelectItem value={"Female"}>Female</SelectItem>
+            <SelectItem value={"Other"}>Other</SelectItem>
+          </Text>
+          :
+          <Text
+            key={id}
+            id={id}
+            label={label}
+            defaultValue={user}
+            ref={refs[id]}
+            variant="standard"
+            InputProps={inputProps}
+          />
+        ))}
+        <MidButton variant="contained" onClick={() => buttonClick()} >{buttonText}</MidButton>
         <MidButton variant="contained" onClick={() => setStatus("home")}>Back</MidButton>
       </Box>
     </>
