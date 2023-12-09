1037a8c73f9f18e44dec1cb6926f7a2da3b11d75
Leo Hsu <leodream251324@gmail.com>
2023-12-09 20:26:03 +0800

    routeController.js

--- backend/controllers/routeController.js
+++ backend/controllers/routeController.js
@@ -21,20 +21,54 @@ const createRoute = async (req, res) => {
         res.status(401).json({ error: "Please sign in before querying driver data"})
         return;
     }
-    // TODO
-    // create boarding with aboard time calculated
-    // loop through all stops to find the distance between each stop
-    // calculate the time between each stop
-
-
-    const routeData = req.body;
+    // I need frontend to send:
+    // 1. startTime
+    // 2. A list of stopIds
+    // 3. available: available seats
+    // 4. type: GO or BACK
+    // 5. state: PROCESSING or CONFIRMED
   
     try {
-      const route = await routeService.createRoute(routeData);
-      const boarding = await 
+      // create route
+      routeInfo = {}
+      routeData.stopIds = routeData.stopIds.sort();
+      if (routeData.type == "GO") {
+        routeInfo.start = routeData.stopIds[0];
+        routeInfo.destination = routeData.stopIds[routeData.stopIds.length - 1];
+      } else {
+        routeInfo.start = routeData.stopIds[routeData.stopIds.length - 1];
+        routeInfo.destination = routeData.stopIds[0];
+      }
+      routeInfo.driverId = driverId;
+      routeInfo.startTime = routeData.startTime;
+      routeInfo.available = routeData.available;
+      routeInfo.type = routeData.type;
+      routeInfo.state = routeData.state;
+
+      const route = await routeService.createRoute(routeInfo);
+
+      // create boardings
+      const total_distance = 0;
+      const arriveTime = routeData.startTime;
+      for (let i = 0; i < routeData.stopIds.length; i++) {
+        const stop = stopService.getStopById(routeData.stopIds[i]);
+        
+        const boarding = {
+          routeId: route.routeID,
+          stopId: routeData.stopIds[i],
+          boardTime: arriveTime
+        } 
+        await boardingService.createBoarding(boarding);
+        
+        if (i < routeData.stopIds.length - 1) {
+          const nextStopId = stopService.getStopById(routeData.stopIds[i + 1]);
+          const distance = distanceCalculator(stop.latitude, stop.longtitude, nextStopId.latitude, nextStopId.longtitude);
+          total_distance += distance;
+          arriveTime = routeData.startTime + total_distance / 40; // 40 km/h
+        }
+      }
 
       res.status(201).json(route);
-      
       
     } catch (error) {
       console.error(error);
