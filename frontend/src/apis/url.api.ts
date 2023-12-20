b025e63f8b2ffc70339d244340de5f39d2c4420c
Leo Hsu <leodream251324@gmail.com>
2023-12-21 00:13:28 +0800

    url.api.ts fix

--- frontend/src/apis/url.api.ts
+++ frontend/src/apis/url.api.ts
@@ -1,3 +1,3 @@
-// export const path = "http://localhost:4000";
-export const path = "https://" + process.env.REACT_APP_BACKEND_URL;
+export const path = "http://localhost:4000";
+// export const path = "https://" + process.env.REACT_APP_BACKEND_URL;
 export const testPath = "http://localhost:8000";
